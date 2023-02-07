9720a8d04cc1f81488c89b0f10b1f4c77a8af734
Ayasha <ayana.kaldybaeva@icloud.com>
2023-02-07 16:23:56 +0600

    Initial commit

---
+++ src/com/company/Accountant.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Accountant {
+    public static void main(String[] args) {
+        int worker1 = 55000;
+        int worker2 = 40000;
+        int worker3 = 70000;
+
+        worker1 = worker1 * 2;
+        worker2 = worker2 + 15000;
+
+        System.out.println(worker1 + "\n" + worker2 + "\n" + worker3);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "John"; //name
+        String surname = "Duke"; //surname
+        String fathersName = "William"; //fathersname
+        byte age = 45; //age
+        String city = "Amsterdam"; //city
+        age = 30; //changed age
+
+        System.out.println("First name: " + name + "\n" + "Last name: " + surname + "\n" + "Father's name: " + fathersName + "\n" + "Age: " + age + "\n" + "City: " + city);
+    }
+}
